2b75d4aeab2f198c5c5020775a8e1b7539895cfc
Regnihov <gapakiis@gmail.com>
2024-04-27 01:56:46 +0200

    magical not visible change

--- Tiles/EmptyTile.gd
+++ Tiles/EmptyTile.gd
@@ -5,7 +5,7 @@ class_name EmptyTile
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	texture = preload("res://images/tile_empty.png")
+	texture = preload("res://images/tile_24x24_empty.png")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
