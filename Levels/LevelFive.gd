994a1dcd39ed47cee0badfc5c4fd1031402fbc72
Regnihov <gapakiis@gmail.com>
2024-04-27 11:22:11 +0200

    level 4 and part of lvl 5

--- Levels/LevelFive.gd
+++ Levels/LevelFive.gd
@@ -3,9 +3,21 @@ extends Map
 class_name LevelFive
 
 func set_dimensions():
-	number_of_tiles_x = 11
-	number_of_tiles_y = 11
+	number_of_tiles_x = 4
+	number_of_tiles_y = 4
 
 func level_definition():
-	var tile = t_tile_script.new()
-	set_tile_at(tile, 2, 2, 3)
+	var tile = input_script.new(colour_script.new(0, 1, 0))
+	set_tile_at(tile, 1, 0, 0)
+	tile = input_script.new(colour_script.new(1, 0, 0))
+	set_tile_at(tile, 3, 1, 1)
+	tile = input_script.new(colour_script.new(1, 1, 0))
+	set_tile_at(tile, 2, 3, 2)
+	tile = input_script.new(colour_script.new(0, 0, 1))
+	set_tile_at(tile, 0, 2, 3)
+	tile = output_script.new(colour_script.new(1, 1, 0))
+	set_tile_at(tile, 3, 2, 3)
+	all_outputs.append(tile)
+	tile = output_script.new(colour_script.new(1, 1, 1))
+	set_tile_at(tile, 1, 3)
+	all_outputs.append(tile)
--- Levels/LevelFour.gd
+++ Levels/LevelFour.gd
@@ -3,9 +3,17 @@ extends Map
 class_name LevelFour
 
 func set_dimensions():
-	number_of_tiles_x = 11
-	number_of_tiles_y = 11
+	number_of_tiles_x = 4
+	number_of_tiles_y = 3
 
 func level_definition():
-	var tile = t_tile_script.new()
-	set_tile_at(tile, 7, 5, 2)
+	var tile = input_script.new(colour_script.new(1, 1, 0))
+	set_tile_at(tile, 1, 0, 0)
+	tile = input_script.new(colour_script.new(0, 0, 1))
+	set_tile_at(tile, 2, 0, 0)
+	tile = output_script.new(colour_script.new(1, 1, 1))
+	set_tile_at(tile, 1, 2)
+	all_outputs.append(tile)
+	tile = output_script.new(colour_script.new(0, 0, 1))
+	set_tile_at(tile, 2, 2)
+	all_outputs.append(tile)
