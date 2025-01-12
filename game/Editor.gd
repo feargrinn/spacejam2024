25c68ed2b2df89070527b91756fbea61c377e000
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-01-12 13:55:14 +0100

    Added field for inputing level name

--- game/Editor.gd
+++ game/Editor.gd
@@ -1,10 +1,15 @@
 extends MarginContainer
 
+var level_name
+
+func _ready() -> void:
+	var name_edit = $VBoxContainer/LevelName
+	name_edit.connect("focus_entered", _on_level_name_focus_entered.bind(name_edit))
+	name_edit.connect("text_submitted", _on_level_name_text_submitted.bind(name_edit))
+	name_edit.connect("focus_exited", _on_level_name_focus_exited.bind(name_edit))
+
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://menu.tscn")
-
-
-
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed == false and event.button_index == MOUSE_BUTTON_RIGHT:
@@ -18,3 +23,14 @@ func _input(event):
 
 func _on_save_pressed() -> void:
 	$VBoxContainer/TileMap.to_level().save_to_file()
+
+
+func _on_level_name_focus_entered(edit_node) -> void:
+	level_name = edit_node.text
+
+func _on_level_name_text_submitted(text, edit_node) -> void:
+	level_name = text
+	edit_node.release_focus()
+
+func _on_level_name_focus_exited(edit_node) -> void:
+	edit_node.text = level_name
