b61f283b0b78070c7f1781ef7bff0427cdd38b67
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-02-27 13:54:13 +0100

    Found how to use the nine patch rect inside button :D

--- game/menu/lever_picker/LevelButton.gd
+++ game/menu/lever_picker/LevelButton.gd
@@ -4,7 +4,4 @@ extends Button
 
 func _init(level_name: String):
 	set_text(level_name)
-	icon = load("res://menu/shared/graphics/buttons/menu_button_lvl_empty.png")
-	set_icon_alignment(HORIZONTAL_ALIGNMENT_CENTER)
-	set_vertical_icon_alignment(VERTICAL_ALIGNMENT_CENTER)
-	set_flat(true)
+	custom_minimum_size = Vector2(64, 64)
